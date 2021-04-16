d2458980deb7cbbb287c7e6541d38947c6b1d037
Andrey1de <andrey1de@gmail.com>
2021-04-16 22:49:30 +0300

    //       app.UseHttpsRedirection();

--- Startup.cs
+++ Startup.cs
@@ -102,7 +102,7 @@ namespace AasCloudData
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AasCloudData v1"));
             }
 
-            app.UseHttpsRedirection();
+     //       app.UseHttpsRedirection();
             app.UseRouting();
 
             app.UseAuthorization();
