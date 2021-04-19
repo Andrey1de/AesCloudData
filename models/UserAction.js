0bfbfd69cfb1cee75d28c5bb5703fb815f5a411a
Andrey1de <andrey1de@gmail.com>
2021-04-19 22:06:41 +0300

    New Zero

--- models/UserAction.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserAction = void 0;
-class UserAction {
-}
-exports.UserAction = UserAction;
-//# sourceMappingURL=UserAction.js.map
